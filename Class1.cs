2ed6e2b90a53bf620f3b24f1fb60fcd92221e9bb
CTJoriginal <48687492+CTJoriginal@users.noreply.github.com>
2022-08-19 12:46:00 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace HumanTimeSpanParser
+{
+    public class HumanTimeSpanParser
+    {
+        public TimeSpan Parse(string input)
+        {
+            TimeSpan ParsedTimeSpan = new();
+            double AmountOfTime;
+
+            string[] split = input.Split(' ');
+
+            foreach (string s in split)
+                s.Trim().ToLower();
+
+            if (!double.TryParse(split[0], out AmountOfTime))
+                throw new ArgumentException("Input string does not specify amount of time");
+
+            switch (split[1])
+            {
+                case string s when s.StartsWith("milis"):
+                    ParsedTimeSpan = TimeSpan.FromMilliseconds(AmountOfTime);
+                    break;
+                case string s when s.StartsWith("sec"):
+                    ParsedTimeSpan = TimeSpan.FromSeconds(AmountOfTime);
+                    break;
+                case string s when s.StartsWith("mi"):
+                    ParsedTimeSpan = TimeSpan.FromMinutes(AmountOfTime);
+                    break;
+                case string s when s.StartsWith("h"):
+                    ParsedTimeSpan = TimeSpan.FromHours(AmountOfTime);
+                    break;
+                case string s when s.StartsWith("d"):
+                    ParsedTimeSpan = TimeSpan.FromDays(AmountOfTime);
+                    break;
+                case string s when s.StartsWith("w"):
+                    ParsedTimeSpan = TimeSpan.FromDays(AmountOfTime * 7);
+                    break;
+                case string s when s.StartsWith("mo"):
+                    ParsedTimeSpan = TimeSpan.FromDays((AmountOfTime * 30));
+                    break;
+                case string s when s.StartsWith("year"):
+                    ParsedTimeSpan = TimeSpan.FromDays(AmountOfTime * 365);
+                    break;
+            }
+
+            return ParsedTimeSpan;
+        }
+    }
+    
+}
